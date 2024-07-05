4f81f1637539f259a817ddb018013d9d4762428f
SamuelFontes <samuguel@hotmail.com.br>
2024-07-05 16:48:30 -0300

    Compiling hello world

--- ex1.asm
+++ ex1.asm
@@ -1,5 +1,5 @@
-global _start #define starting point
-_start: # create lable to use goto
-	mov eax, 1 # we are making a system exit call, signal end of the program
-	mov ebx, 42 # exit status for the program
-	int 0x80 #interrupt handler?? this is for system call
+global _start 
+_start: 
+	mov eax, 1 
+	mov ebx, 42 
+	int 0x80 
