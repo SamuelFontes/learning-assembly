ead464ec66793a0f0927ddc12ac61a5b337f2619
SamuelFontes <samuguel@hotmail.com.br>
2024-07-08 13:10:37 -0300

    Using jmp to jump around

--- main.asm
+++ main.asm
@@ -1,10 +1,10 @@
 ; EX1
 ;global _start 
-;_start: 
-;mov eax, 1 ; a
-;mov ebx, 42 
-;sub ebx, 2
-;int 0x80 
+_start1: 
+	mov eax, 1  	; exit system call
+	mov ebx, 42 	; exit code
+	sub ebx, 2	; subtract 2 from exit code ebx
+	int 0x80 
 
 ; EX2
 global _start
@@ -14,9 +14,22 @@ section .data
 	len equ $ - msg
 
 section .text
-_start:
+_start2:
 	mov eax, 4	; sys_write system call
 	mov ebx, 1	; stdout file descriptor
 	mov ecx, msg	; message content
 	mov edx, len	; message size
 	int 0x80	; perform system call, execute code
+
+	mov eax, 1	; system call to exit the program
+	mov ebx, 0	; set the exit code
+	int 0x80	; run exit system call
+
+; EX3
+; EIP instruction pointer, holds location of machine code the cpu is executing, we can jump around the code by changing this pointer
+
+_start:
+	mov ebx, 42	; exit code
+	mov eax, 1	; set to exit system call
+	jmp _start1
+	int 0x80
